bb9bfe36553208c3b40183a3e5c3f3648d977f9e
cramallo <cramallo65@gmail.com>
2019-05-01 18:19:12 -0300

    cambios en comment model

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 var commentSchema = new Schema({
     
-    user: { type: Schema.Types.ObjectId, ref:'User'},
+    user: { type: Schema.Types.ObjectId, ref:'User',required:[true,'the user id is required']},
     show: { type: Schema.Types.ObjectId, ref:'Show', required:[true,'the movie id is required']},
     imbdID:{ type:String},
     score: { type: Number, required:[true,"the score is required"] },
@@ -12,4 +12,4 @@ var commentSchema = new Schema({
     
 });
 
-  module.exports = mongoose.model('Comment',commentSchema);
+module.exports = mongoose.model('Comment',commentSchema);
